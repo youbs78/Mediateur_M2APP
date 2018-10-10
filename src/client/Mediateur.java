018832b7739b8f3a32d6a490ae8feda147d987ba
Durand Kévin <kevindrnd@live.fr>
2018-10-10 10:38:18 +0200

    mediateur to main

---
+++ src/client/Mediateur.java
@@ -0,0 +1,12 @@
+package client;
+
+import model.Oracle;
+
+public class Mediateur {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        Oracle sourceOracle = new Oracle();
+
+    }
+}
