61c367383ad985f81dcc38ce9c30ef58bad439a3
Durand Kévin <kevindrnd@live.fr>
2018-10-16 14:12:28 +0200

    Classe test ajouté pour Excel

---
+++ Test/model/ExcelTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExcelTest {
+
+    @Test
+    public void connexion() {
+    }
+
+    @Test
+    public void deconnexion() {
+    }
+
+    @Test
+    public void displaySQLErrors() {
+    }
+
+    @Test
+    public void requeteSQL() {
+    }
+}
