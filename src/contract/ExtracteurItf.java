de13f26885bb5508a917f767abf8496c5166eee5
36010082 <36010082@miage-210-016.dmiage.parisnanterre.fr>
2018-10-16 17:54:26 +0200

    Interface Med & Extract

--- src/contract/ExtracteurItf.java
+++ src/contract/ExtracteurItf.java
@@ -5,30 +5,34 @@ import domain.Cours;
 import java.util.List;
 
 public interface ExtracteurItf {
-    //Méthode/fonction de connexion et déconnexion à la source de données crée préalablement qui suit un schéma conceptuel décrit dans un catalogue (dans la BDD/fichier) ;
+	// Générer une table de correspondance entre la source et le médiateur pour 
+    //décrire le template des attributs ; (HASHMAP ==>  Cle: Requete Mediateur , Valeur : Requete Source)
+    //hashmap<String,String> TableCorespond;
+	
+	
+	//Méthode/fonction de connexion et déconnexion à la source de données crée préalablement qui suit un schéma conceptuel décrit dans un catalogue (dans la BDD/fichier) ;
     boolean connexion();
     boolean deconnexion();
+    
 
-    //Permettre l'envoi et la réception de requêtes depuis/vers le médiateur
-    String getSrcReq();
     // Méthode/fonction pour recevoir les requêtes du médiateur ;
-    void setMediateurReq(String req);
-
-    // Générer une table de correspondance entre la source et le médiateur pour décrire le template des attributs ;
-    void genererTableCorr();
-
+    void setMediateurReq(String reqMed);
+    
+    
     // Méthode/fonction pour traduire la requête (vue) du médiateur par le schéma de la source
     // en parcourant la table de correspondance générée par le générateur d’adaptateur
     // et chercher le template qui correspond à la requête du médiateur ;
-    String reqMedtoReqSrc();
-
+    String reqMedtoReqSrc(String reqMed);
+    
     // Méthode/fonction pour exécuter (interroger la source) les requêtes du médiateur sur la source ;
-    void executeReq(String req);
-
+    void executeReq(String reqSrc);
+    
     // Méthode/fonction pour récupérer le résultat de la requête
-    void getResFromExecuteReq(String req);
-
+    List<Object> getResFromExecuteReq(String req);
+    
     // Méthode/fonction pour traduire le résultat et l’envoyer au médiateur.
-    void tradResToMed();
+    List<Object> tradResToMed(List<Object> resSrc);
+    
+   
 
 }
--- src/contract/MediateurItf.java
+++ src/contract/MediateurItf.java
@@ -1,15 +1,21 @@
 package contract;
 
+import java.util.List;
+
 public interface MediateurItf {
     // Définir les requêtes (vues) en se basant sur le dictionnaire de données du schéma global du médiateur ;
-
+//	List<Object> src1
+//	List<Object> src2
+//	List<Object> src
+//	
     // Méthode/fonction pour envoyer les requêtes aux sources de données ;
-    void sendReq();
+    void sendReq(String reqMed);
 
-    // Méthode/fonction pour récupérer les résultats des 3 sources ;
+    // Méthode/fonction pour récupérer le résultat des 3 sources ;
     void getResult();
 
     // Méthode/fonction pour agréger les résultats des 3 sources.
-    void agregate();
+    List<Object> agregate();
+    
 }
 
