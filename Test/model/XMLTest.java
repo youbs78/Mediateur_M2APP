ceca1c316fd7202ffe62587b7acb903af2155d5d
Durand Kévin <kevindrnd@live.fr>
2018-10-16 14:11:27 +0200

    XML test passé

--- Test/model/XMLTest.java
+++ Test/model/XMLTest.java
@@ -4,17 +4,17 @@ import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
-
-import static org.junit.Assert.*;
 
 public class XMLTest {
     private XML srcXML = new XML();
 
     @Test
     public void lire_XML() {
+        // TODO: Je ne sais plus comment faire des assert pour faire échouer les tests mdr
         try {
-            this.srcXML.lire_XML("Univ_BD_3.xml");
+            this.srcXML.lire_XML("src/data/Univ_BD_3.xml");
         } catch (SAXException | IOException | ParserConfigurationException e) {
             e.printStackTrace();
         }
